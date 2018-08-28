3bf40fc4426beb8cf116ead638664c5f96e12c15
Harsha Kethineni <harshakethineni@flywheel.io>
2018-08-28 16:08:46 -0500

    logging and exit on tagging failure

--- tagger.py
+++ tagger.py
@@ -11,34 +11,36 @@ def add_private_tag(dicomfile, group, identifier, tag_value):
     for elem_tag in range(0x0010, 0x00FF):
         dataelem = dicomfile.get((group, elem_tag))
         if dataelem:
-            is_private_creator = dataelem.tag.group == group and 0x0010 <= dataelem.tag.element <= 0x00ff
-            if is_private_creator and dataelem.value.lower() == identifier.lower():
+            if dataelem.value.lower() == identifier.lower():
                 for private_tag_element in range(dataelem.tag.elem * 0x0100, dataelem.tag.elem * 0x0100 + 0x0100):
                     private_tag = (dataelem.tag.group, private_tag_element)
                     private_elem = dicomfile.get(private_tag)
                     if not private_elem:
                         dicomfile.add_new(private_tag, 'LO', tag_value)
-                        return dicomfile
+                        return dicomfile, private_tag
                     elif private_elem.value.lower().startswith(tag_value.lower()):
-                        return dicomfile
+                        return dicomfile, private_tag
         else:
             identifier_tag = (group, elem_tag)
             dicomfile.add_new(identifier_tag, 'LO', identifier)
             private_tag = (group, elem_tag * 0x0100)
             dicomfile.add_new(private_tag, 'LO', tag_value)
-            return dicomfile
+            return dicomfile, private_tag
     raise TagError('No free element in group {} to tag the dicom'.format(group))
 
 
 def tag_image(file_path, group, identifier, tag_value):
-    dicomfile = add_private_tag(pydicom.dcmread(file_path), group, identifier, tag_value)
+    dicomfile, private_tag = add_private_tag(pydicom.dcmread(file_path), group, identifier, tag_value)
     dicomfile.save_as(file_path)
+    return private_tag
 
 
 def tag_folder(dir_path, group, identifier, tag_value):
+    tags = []
     for image_file in os.listdir(dir_path):
         if '.dcm' in image_file:
-            tag_image(os.path.join(dir_path, image_file), group, identifier, tag_value)
+            tags.append(tag_image(os.path.join(dir_path, image_file), group, identifier, tag_value))
+    return list(set(tags))
 
 
 if __name__ == '__main__':
@@ -52,6 +54,11 @@ if __name__ == '__main__':
     args.group = int(args.group, 16)
 
     if os.path.isdir(args.dicom):
-        tag_folder(args.dicom, args.group, args.identifier, args.tag_value)
+        tags = tag_folder(args.dicom, args.group, args.identifier, args.tag_value)
     else:
-        tag_image(args.dicom, args.group, args.identifier, args.tag_value)
+        tags = [tag_image(args.dicom, args.group, args.identifier, args.tag_value)]
+
+    if len(tags) > 1:
+        print('WARNING: Tagged at different elements')
+    else:
+        print('INFO: Tagged at ({0:#x}, {1:#x})'.format(tags[0][0], tags[0][1]))
