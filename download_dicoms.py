f47d4e9a0ce717e611d0784aa8a008d390b9495b
Margaret Mahan <maggiemahan@flywheel.io>
2020-07-15 15:33:15 -0500

    MAIN: Blackened.

--- download_dicoms.py
+++ download_dicoms.py
@@ -8,24 +8,36 @@ def get_dicoms(fw, session, input_dir):
     if not os.path.exists(input_dir):
         os.makedirs(input_dir)
     for acq in acquisitions:
-        for file_ in acq.get('files', []):
-            if file_.type == 'dicom':
-                fw.download_file_from_acquisition(acq.id, file_.name,
-                                                  '{}/{}'.format(input_dir, file_.name))
+        for file_ in acq.get("files", []):
+            if file_.type == "dicom":
+                fw.download_file_from_acquisition(
+                    acq.id, file_.name, "{}/{}".format(input_dir, file_.name)
+                )
 
 
 def main():
     # Read in arguments
-    parser = argparse.ArgumentParser(description='BIDS Curation')
-    parser.add_argument('--api-key', dest='api_key', action='store',
-                        required=True, help='API key')
+    parser = argparse.ArgumentParser(description="BIDS Curation")
+    parser.add_argument(
+        "--api-key", dest="api_key", action="store", required=True, help="API key"
+    )
 
-    parser.add_argument('--session', dest='session', action='store',
-                        required=False, default=None,
-                        help='Session Id to grab dicoms from')
-    parser.add_argument('--input_dir', dest='input_dir', action='store',
-                        required=False, default=None,
-                        help='Folder to place dicoms in')
+    parser.add_argument(
+        "--session",
+        dest="session",
+        action="store",
+        required=False,
+        default=None,
+        help="Session Id to grab dicoms from",
+    )
+    parser.add_argument(
+        "--input_dir",
+        dest="input_dir",
+        action="store",
+        required=False,
+        default=None,
+        help="Folder to place dicoms in",
+    )
     args = parser.parse_args()
 
     # Prep
@@ -33,11 +45,12 @@ def main():
     fw = flywheel.Flywheel(args.api_key)
     user = fw.get_current_user()
     if user.root:
-        print('Using site admin priviledges to download')
+        print("Using site admin priviledges to download")
         fw = flywheel.Flywheel(args.api_key, root=True)
 
     # Download dicoms
     get_dicoms(fw, args.session, args.input_dir)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/test_tagger.py
+++ tests/test_tagger.py
@@ -7,16 +7,16 @@ import tagger
 class taggerTestCase(unittest.TestCase):
     def setUp(self):
         self.group = 0x0021
-        self.identifier = 'Flywheel'
-        self.tag_value = 'DICOM Send'
+        self.identifier = "Flywheel"
+        self.tag_value = "DICOM Send"
 
     def test_add_private_tag(self):
         ds = pydicom.Dataset()
         tagger.add_private_tag(ds, self.group, self.identifier, self.tag_value)
         identifier_elem = ds.get((self.group, 0x0010))
         private_elem = ds.get((self.group, 0x1000))
-        self.assertTrue(identifier_elem.value == 'Flywheel')
-        self.assertTrue(private_elem.value == 'DICOM Send')
+        self.assertTrue(identifier_elem.value == "Flywheel")
+        self.assertTrue(private_elem.value == "DICOM Send")
 
     def test_tagged_once(self):
         ds = pydicom.Dataset()
@@ -31,26 +31,38 @@ class taggerTestCase(unittest.TestCase):
     def test_identifier_but_no_tag(self):
         # Test when tag isn't there but identifier is
         ds = pydicom.Dataset()
-        ds.add_new((0x0021, 0x0010), 'LO', 'Flywheel')
+        ds.add_new((0x0021, 0x0010), "LO", "Flywheel")
         tagger.add_private_tag(ds, self.group, self.identifier, self.tag_value)
         identifier_elem = ds.get((self.group, 0x0010))
         private_elem = ds.get((self.group, 0x1000))
-        self.assertTrue(identifier_elem.value == 'Flywheel')
-        self.assertTrue(private_elem.value == 'DICOM Send')
+        self.assertTrue(identifier_elem.value == "Flywheel")
+        self.assertTrue(private_elem.value == "DICOM Send")
         for tag in range(0x0011, 0x00FF):
             self.assertFalse(ds.get((self.group, tag)))
 
     def test_no_space_for_identifier(self):
         ds = pydicom.Dataset()
         for tag in range(0x0010, 0x00FF):
-            ds.add_new((0x0021, tag), 'LO', 'Not Flywheel')
-        self.assertRaises(tagger.TagError, tagger.add_private_tag, ds, self.group,
-                          self.identifier, self.tag_value)
+            ds.add_new((0x0021, tag), "LO", "Not Flywheel")
+        self.assertRaises(
+            tagger.TagError,
+            tagger.add_private_tag,
+            ds,
+            self.group,
+            self.identifier,
+            self.tag_value,
+        )
 
     def no_space_for_tag(self):
         ds = pydicom.Dataset()
-        ds.add_new((0x0021, 0x0010), 'LO', 'Flywheel')
+        ds.add_new((0x0021, 0x0010), "LO", "Flywheel")
         for tag in range(0x1000, 0x10FF):
-            ds.add_new((0x0021, tag), 'LO', 'Not DICOM Send')
-        self.assertRaises(tagger.TagError, tagger.add_private_tag, ds, self.group,
-                          self.identifier, self.tag_value)
+            ds.add_new((0x0021, tag), "LO", "Not DICOM Send")
+        self.assertRaises(
+            tagger.TagError,
+            tagger.add_private_tag,
+            ds,
+            self.group,
+            self.identifier,
+            self.tag_value,
+        )
